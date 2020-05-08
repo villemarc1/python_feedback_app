67cab69c22a110a84a3a485fc90af1feda59c211
villemarc1 <47375397+villemarc1@users.noreply.github.com>
2020-05-07 22:28:28 -0300

    Update app.py
    xx

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
 from flask import Flask,jsonify,request,render_template
 
 app = Flask(__name__)
-
-stores = [{
-    'name': 'My Store',
-    'items': [{'name':'my item', 'price': 15.99 }]
-}]
+app.debug = False
 
 
 @app.route('/')
 def home():
   return render_template('index.html')
 
-app.run(port=5000)
+app.run()
